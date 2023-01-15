8297ba4d0de0c10be76f7d6e3f8664e56ccf6ff1
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-01-15 11:28:50 +0530

    h2 database configured

--- src/main/java/com/aman/library/springbootlibrary/entity/Library.java
+++ src/main/java/com/aman/library/springbootlibrary/entity/Library.java
@@ -1,5 +1,22 @@
 package com.aman.library.springbootlibrary.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Library {
+	@Id
+	private int id;
+	private String name;
+	private String commaSeparatedBookName;
 
 }
